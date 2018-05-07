d6f83c911d55f63908fc968188c868e33ad916f9
Kathrin <kathrin@kathrins-air.codefactoryvienna.local>
2018-05-07 11:15:01 +0200

    interfiles

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,35 @@
+
+
+$(document).ready(function() {
+
 $("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
+
 	
 
 });
+
+//$("form").remove(); 
+
+
+for(var i=0; i< 5; i++) {
+	$("form#myform").after("<div id=newdiv>neue Div</div>");
+	$("#newdiv").addClass("box");
+}
+
+
+$("input[name=msg]").val("Search for..."); 
+
+$("a").attr("href", "http://www.codefactory.wien"); 
+
+
+
+
+}); 
+
+
+
+
+
+
